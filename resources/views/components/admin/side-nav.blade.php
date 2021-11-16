7f5b970fc41ccea3ad12ea1f99b176abc95feb7e
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-11-16 18:10:50 +0200

    Added admin layout

---
+++ resources/views/components/admin/side-nav.blade.php
@@ -0,0 +1,5 @@
+<div class="p-2 nav flex-column nav-pills" aria-orientation="vertical">
+    <a class="nav-link {{ $currentPage == 'articles' ? 'active' : '' }}" data-toggle="pill" href="#v-pills-home">Articles</a>
+    <a class="nav-link {{ $currentPage == 'categories' ? 'active' : '' }}" data-toggle="pill" href="#v-pills-profile">Categories</a>
+    <a class="nav-link {{ $currentPage == 'tags' ? 'active' : '' }}" data-toggle="pill" href="#v-pills-messages">Tags</a>
+</div>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }} - Admin Panel
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Logout Link -->
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        
+
+
+
+        <div class="row">
+            <div class="col-2">
+                <x-admin.side-nav :currentPage="$currentPage"></x-admin.side-nav>
+            </div>
+
+            <div class="col-10">
+                Hello
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</body>
+</html>
