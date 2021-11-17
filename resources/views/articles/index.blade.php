f54fdaac013d6c7991c9233207b6356a1aa51a79
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-11-17 17:05:43 +0200

    Added a link in the article to the author

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -14,7 +14,7 @@
             <div class="card-body">
                 <a href="{{ route('category.show', $article->category) }}" class="btn btn-light mb-2">{{ $article->category->name }}</a>
                 <h5 class="card-title">{{ $article->title }}</h5>
-                <h6 class="card-subtitle text-muted">Posted {{ $article->created_at->diffForHumans() }} by <a href="#">{{ $article->user->name }}</a></h6>
+                <h6 class="card-subtitle text-muted">Posted {{ $article->created_at->diffForHumans() }} by <a href="{{ route('user.show', $article->user) }}">{{ $article->user->name }}</a></h6>
                 <br>
                 <p class="card-text">{{ Str::limit($article->body, 256) }}</p>
                 <a href="{{ route('article.show', $article) }}" class="btn btn-primary">Continue Reading</a>
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,7 +8,7 @@
         <img src="https://picsum.photos/1000/1000" class="img-fluid card-img-top" style="max-height: 250px; object-fit: cover;">
         <div class="card-body">
             <h5 class="card-title">{{ $article->title }}</h5>
-            <h6 class="card-subtitle text-muted">Posted {{ $article->created_at->diffForHumans() }} by <a href="#">{{ $article->user->name }}</a></h6>
+            <h6 class="card-subtitle text-muted">Posted {{ $article->created_at->diffForHumans() }} by <a href="{{ route('user.show', $article->user) }}">{{ $article->user->name }}</a></h6>
             <br>
             <p class="card-text">{!! nl2br(e($article->body)) !!}</p>
         </div>
