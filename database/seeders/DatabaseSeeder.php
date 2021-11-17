3a2248e64540dd42d1998ae2f16f6f78c5ec820e
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-11-17 07:31:38 +0200

    Created Tag model, migration and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\Tag;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        Category::factory(5)->create();
+        //Category::factory(5)->create();
+        Tag::factory(3)->create();
     }
 }
