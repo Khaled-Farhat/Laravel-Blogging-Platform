7574d380839f1a2efa54da368b08ee6ed90975bc
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-11-18 09:34:01 +0200

    Modified displaying the category name in the article

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -12,12 +12,15 @@
         <div class="card mb-3">
             <img src="https://picsum.photos/1000/1000" class="img-fluid card-img-top" style="max-height: 250px; object-fit: cover;">
             <div class="card-body">
-                @isset($article->category)
-                    <a href="{{ route('categories.show', $article->category) }}" class="btn btn-light mb-2">{{ $article->category->name }}</a>
-                @endisset
-
                 <h5 class="card-title">{{ $article->title }}</h5>
-                <h6 class="card-subtitle text-muted">Posted {{ $article->created_at->diffForHumans() }} by <a href="{{ route('users.show', $article->user) }}">{{ $article->user->name }}</a></h6>
+                <h6 class="card-subtitle text-muted">
+                    Posted {{ $article->created_at->diffForHumans() }}
+                    by <a href="{{ route('users.show', $article->user) }}">{{ $article->user->name }}</a>
+                    @isset($article->category)
+                    in category: <a href="{{ route('categories.show', $article->category) }}">{{ $article->category->name }}</a>
+                    @endisset
+
+                </h6>
                 <br>
                 <p class="card-text">{{ Str::limit($article->body, 256) }}</p>
                 <a href="{{ route('articles.show', $article) }}" class="btn btn-primary">Continue Reading</a>
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,7 +8,13 @@
         <img src="https://picsum.photos/1000/1000" class="img-fluid card-img-top" style="max-height: 250px; object-fit: cover;">
         <div class="card-body">
             <h5 class="card-title">{{ $article->title }}</h5>
-            <h6 class="card-subtitle text-muted">Posted {{ $article->created_at->diffForHumans() }} by <a href="{{ route('users.show', $article->user) }}">{{ $article->user->name }}</a></h6>
+            <h6 class="card-subtitle text-muted">
+                Posted {{ $article->created_at->diffForHumans() }}
+                by <a href="{{ route('users.show', $article->user) }}">{{ $article->user->name }}</a>
+                @isset($article->category)
+                in category: <a href="{{ route('categories.show', $article->category) }}">{{ $article->category->name }}</a>
+                @endisset
+            </h6>
             <br>
             <p class="card-text">{!! nl2br(e($article->body)) !!}</p>
         </div>
