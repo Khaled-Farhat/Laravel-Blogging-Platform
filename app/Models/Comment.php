7ef717886fe06455163c33c625dc1ce4efb120bb
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-11-19 10:06:11 +0200

    Added Reviewed global scope to Comment model and implemented some local scopes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,12 +2,38 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $attributes = [
+        'reviewed_at' => null,
+    ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('reviewed', function(Builder $builder) {
+            $builder->whereNotNull('reviewed_at');
+        });
+    }
+
+    public function scopeOnlyPendingReview($query)
+    {
+        $query->withoutGlobalScope('reviewed')->whereNull('reviewed_at');
+    }
+
+    public function scopeWithPendingReview($query)
+    {
+        $query->withoutGlobalScope('reviewed');
+    }
+
+    public function review() {
+        $this->reviewed_at = now();
+    }
 
     public function article()
     {
