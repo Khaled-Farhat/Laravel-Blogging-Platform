a9464b8b83cf80bc4f1fc3e7a7daec1ab10f20e9
Khaled Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2021-12-22 19:36:18 +0200

    Allowed only admin users to view the users page

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -17,7 +17,7 @@ class UserPolicy
      */
     public function viewAny(User $user)
     {
-        return true;
+        return $user->hasRole('Admin');
     }
 
     /**
@@ -29,7 +29,7 @@ class UserPolicy
      */
     public function view(User $user, User $model)
     {
-        return true;
+        return $user->hasRole('Admin');
     }
 
     /**
--- resources/views/components/admin/side-nav.blade.php
+++ resources/views/components/admin/side-nav.blade.php
@@ -3,5 +3,7 @@
     <a class="list-group-item list-group-item-action {{ $currentPage == 'categories' ? 'active' : '' }}" href="{{ route('admin.categories.index') }}" role="tab">Categories</a>
     <a class="list-group-item list-group-item-action {{ $currentPage == 'comments' ? 'active' : '' }}" href="{{ route('admin.comments.index') }}" role="tab">Comments</a>
     <a class="list-group-item list-group-item-action {{ $currentPage == 'tags' ? 'active' : '' }}" href="{{ route('admin.tags.index') }}" role="tab">Tags</a>
-    <a class="list-group-item list-group-item-action {{ $currentPage == 'users' ? 'active' : '' }}" href="{{ route('admin.users.index') }}" role="tab">Users</a>
+    @can('viewAny', App\Models\User::class)
+        <a class="list-group-item list-group-item-action {{ $currentPage == 'users' ? 'active' : '' }}" href="{{ route('admin.users.index') }}" role="tab">Users</a>
+    @endcan
 </div>
